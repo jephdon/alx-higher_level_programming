70039926820598c276995bdc3e6d8da95873a8fa
jephdon <chukwujekwujephthah@gmail.com>
2025-09-11 16:40:27 -0700

    Script takes in an argument and displays all values in the states table of hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A script to query a MySQL database for states matching a given name.
+
+This script connects to a MySQL database using the provided credentials
+and database name, and prints all states where the name matches the
+grgument, sorted by id in ascending order.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
