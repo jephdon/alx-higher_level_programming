1aa5c752001e7362577001cd66620280fe669d7f
jephdon <chukwujekwujephthah@gmail.com>
2025-09-13 10:19:13 -0700

    Bash script takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body
-# of a 200 status response
+# GET request to a URL and displays the body of a 200 status response
 curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
